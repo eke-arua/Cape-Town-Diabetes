cfe7d1e0c82e9616bf423a4c97b91c62f4574e0b
Eke Arua <aruaneke@gmail.com>
2025-04-15 14:37:25 +0200

    Update

--- LGCP fitting.R
+++ LGCP fitting.R
@@ -13,3 +13,8 @@ facility_dist <- project(facility_dist, "EPSG:22234")
 
 #Population density as the baseline (offset)
 baseline <- classify(scale(pop_density), cbind(-Inf, 0, 0.0001), include.lowest = TRUE)
+
+#Load the point simulated point pattern data
+mod_list <- qs2::qs_read(here("500 factorial design simulations.qs2"))
+
+
