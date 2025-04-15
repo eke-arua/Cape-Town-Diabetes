6f65a52341c79930dbd146b9b6dddd7e242c68f4
Eke Arua <aruaneke@gmail.com>
2025-04-15 14:17:02 +0200

    Initial commit

---
+++ LGCP fitting.R
@@ -0,0 +1,15 @@
+library(spatstat)
+library(here)
+library(terra)
+library(scampr)
+library(qs2)
+
+#Load the covariates
+pop_density <- terra::rast("Cape Town population density.tif")
+pop_density <- project(pop_density, "EPSG:22234")
+
+facility_dist <- rast("Distance to healthcare facility.tif")
+facility_dist <- project(facility_dist, "EPSG:22234")
+
+#Population density as the baseline (offset)
+baseline <- classify(scale(pop_density), cbind(-Inf, 0, 0.0001), include.lowest = TRUE)
